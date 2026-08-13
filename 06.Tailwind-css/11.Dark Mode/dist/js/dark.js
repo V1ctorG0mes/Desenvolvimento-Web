c55f6b4fdde47e27c5639cccaac2dfe435f67386
V1ctorG0mes <vhenrique2020@hotmail.com>
2026-08-13 14:34:45 -0300

    Tailwind Css and Updates

---
+++ 06.Tailwind-css/11.Dark Mode/dist/js/dark.js
@@ -0,0 +1,6 @@
+const htmlElement = document.querySelector('html');
+const btnTheme = document.querySelector('#btn-theme')
+
+btnTheme.addEventListener('click', () => {
+    htmlElement.classList.toggle('dark')
+})
